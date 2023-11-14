a3d3e9e828ca38eb8d9f3f8c3d4fd48d0f496894
Mitantsoaa <mitaranaivo@gmail.com>
2023-11-14 07:22:10 +0300

    feat/ models method

---
+++ models/Personne.php
@@ -0,0 +1,77 @@
+<?php
+require_once 'connexion.php';
+
+class Personne {
+
+    public function getAllPerson($order = 'nom', $paginate, $start_from)
+    {
+        try {
+            $pdo = DataBase::connect();
+            $sql = $pdo->prepare("SELECT * FROM personnel p GROUP BY p.id ORDER BY $order LIMIT $start_from, $paginate");
+            $sql->execute();
+            $result = $sql->fetchAll();
+            DataBase::disconnect();
+        } catch (PDOException  $e ){
+            echo "Error: ".$e;
+        }
+        return $result;
+    }
+
+    public function getPersonById($id) {
+        try{
+            $pdo = DataBase::connect();
+            $sql = $pdo->prepare("SELECT * FROM personnel WHERE id = $id");
+            $sql->execute();
+            $result = $sql->fetch();
+            DataBase::disconnect();
+        }catch(PDOException  $e ){
+            echo "Error: ".$e;
+        }
+        return ($result);
+    }
+
+
+
+     public function updatePerson( $id, $img, $nom, $mail, $phone) {
+         try {
+             $pdo = DataBase::connect();
+             $sql = $pdo->prepare(
+                 "UPDATE personnel SET img_url = ?, nom = ?, email=?, phone=? WHERE id = $id");
+             $sql->execute([$img, $nom, $mail, $phone]);
+             $sql->execute();
+             DataBase::disconnect();;
+         } catch (Exception $e) {
+             DataBase::disconnect();
+             throw $e;
+         }
+    }
+
+    public function deletePerson( $id) {
+         try {
+             $pdo = DataBase::connect();
+             $sql = $pdo->prepare("DELETE FROM personnel WHERE id = $id");
+             $sql->execute();
+             DataBase::disconnect();
+         } catch (Exception $e) {
+             DataBase::disconnect();
+             throw $e;
+         }
+    }
+
+    public function paginator ($limit)
+    {
+        try {
+            $pdo = DataBase::connect();
+            $sth = $pdo->prepare("SELECT COUNT(id) FROM personnel");
+            $sth->execute();
+            $result = $sth->fetchColumn();
+
+            DataBase::disconnect();
+            $total_pages = ceil($result / $limit);
+            return $total_pages;
+        } catch (PDOException  $e ){
+            echo "Error: ".$e;
+        }
+    }
+
+}
