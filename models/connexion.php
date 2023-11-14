3aceed54ea9884a913149ae1aead8d1660659fc7
Mitantsoaa <mitaranaivo@gmail.com>
2023-11-14 06:44:02 +0300

    feat/ database connexion

---
+++ models/connexion.php
@@ -0,0 +1,39 @@
+<?php
+
+class DataBase extends PDO
+{
+    private static $dbName = 'eval' ;
+    private static $dbHost = 'localhost' ;
+    private static $dbUsername = 'root';
+    private static $dbUserPassword = '';
+
+    private static $con  = null;
+
+    public function __construct()
+    {
+        die('Init function is not allowed');
+    }
+
+    public static function connect()
+    {
+        if ( null == self::$con )
+    {
+    try
+        {
+        self::$con =  new PDO( "mysql:host=".self::$dbHost.";"."dbname=".self::$dbName, self::$dbUsername, self::$dbUserPassword);
+
+        }
+    catch(PDOException $e)
+        {
+        die($e->getMessage());
+        }
+    }
+    return self::$con;
+    }
+
+    public static function disconnect()
+    {
+        self::$con = null;
+    }
+
+}
